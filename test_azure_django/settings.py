9a8bf4475b99ccd87e5b00b53c335105a7260996
Adam Saladino <asaladino@risklogic.com>
2022-01-04 13:43:23 -0500

    problems.

--- test_azure_django/settings.py
+++ test_azure_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-$@7e@pbmm6ulntl7+7gxcsfnr%op8!^j8^jjrd#5bq9ka5)97v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
